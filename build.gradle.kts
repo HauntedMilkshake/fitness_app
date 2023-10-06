1a7a55b6435a82eb79060496306bde462a9e514b
HauntedMilkshake <spaszahov@gmail.com>
2023-10-06 21:29:45 +0300

    firebase linked + dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,9 @@
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
 plugins {
-    id("com.android.application") version "8.1.0" apply false
+    id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
