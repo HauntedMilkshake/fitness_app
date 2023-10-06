b64fbf6c583c51e17faf999db00dc0dd041eea0f
Spas Zahov <spaszahov@gmail.com>
2023-10-06 10:42:01 +0300

    boilerplates for fragments, fixed a bug when the app launches, added icons for bottom nav

---
+++ app/src/main/java/com/example/fitness_app/exercise/FragmentExercises.kt
@@ -0,0 +1,20 @@
+package com.example.fitness_app.exercise
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fitness_app.databinding.FragmentExercisesBinding
+
+class FragmentExercises: Fragment() {
+    private var _binding: FragmentExercisesBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentExercisesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/fitness_app/login/FragmentLogIn.kt
+++ app/src/main/java/com/example/fitness_app/login/FragmentLogIn.kt
@@ -10,15 +10,19 @@ import com.example.fitness_app.databinding.FragmentLogInBinding
 class FragmentLogIn: Fragment() {
     private var _binding: FragmentLogInBinding? = null
     private val binding get() = _binding!!
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentLogInBinding.inflate(inflater, container, false)
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //for hiding the navbar, haven't tested it
+//        val view = requireActivity().findViewById<BottomNavigationView>(R.id.nav_view)
+//
+//        val fab = requireActivity().findViewById<FloatingActionButton>(R.id.fab)
+//        fab.visibility = View.GONE
+//        view.visibility = View.GONE
 
     }
     override fun onDestroyView() {
