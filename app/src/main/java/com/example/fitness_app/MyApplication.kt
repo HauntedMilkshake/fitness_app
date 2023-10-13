d745dfa1f9704030731aac9188790213715d3b85
HauntedMilkshake <spaszahov@gmail.com>
2023-10-13 18:06:57 +0300

    commiting some little tweaks

---
+++ app/src/main/java/com/example/fitness_app/MyApplication.kt
@@ -0,0 +1,10 @@
+package com.example.fitness_app
+
+import android.app.Application
+
+class MyApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ buildscript {
     }
 }
 plugins {
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
