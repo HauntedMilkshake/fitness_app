e97aa7380f4eb2acec5a380b010b23d8a842f509
Spas Zahov <spaszahov@gmail.com>
2023-10-27 20:55:42 +0300

    some minor touches to the ui, started adding login and signup logic

---
+++ app/src/main/java/com/example/fitness_app/login/LoginViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.fitness_app.login//package com.example.fitness_app.login
+//
+//import androidx.lifecycle.LiveData
+//import androidx.lifecycle.MutableLiveData
+//import androidx.lifecycle.ViewModel
+////might be wrong
+//import com.google.firebase.database.core.view.View
+//
+//class LoginViewModel: ViewModel() {
+//    private val _isLoggedIn = MutableLiveData<Boolean>()
+//    val isLoggedIn: LiveData<Boolean> get() = _isLoggedIn
+//    init{
+//        _isLoggedIn.value = false
+//    }
+//    fun login(email: String, password: String, context: View){
+//        Firebase.auth.log
+//    }
+//}
