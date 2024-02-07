71c015f56408b775d782931cacd179aec2f7df84
Spas Zahov <spaszahov@gmail.com>
2024-02-07 17:18:31 +0200

    recycler view works, working on swipe, switching machines

--- app/src/main/java/bg/zahov/app/ui/workout/add/ExerciseSetAdapter.kt
+++ app/src/main/java/bg/zahov/app/ui/workout/add/ExerciseSetAdapter.kt
@@ -1,9 +1,12 @@
 package bg.zahov.app.ui.workout.add
 
 import android.util.Log
+import android.view.GestureDetector
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import bg.zahov.app.data.model.ClickableSet
 import bg.zahov.app.data.model.Exercise
@@ -16,9 +19,9 @@ import com.google.android.material.imageview.ShapeableImageView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.google.android.material.textview.MaterialTextView
+import kotlin.math.abs
 
-class ExerciseSetAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(),
-    SwipeGesture.OnSwipeListener {
+class ExerciseSetAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     companion object {
         const val VIEW_TYPE_EXERCISE = 1
@@ -66,7 +69,7 @@ class ExerciseSetAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(),
             }
 
             is SetViewHolder -> {
-                holder.bind((items[position] as SetEntry).set)
+                holder.bind((items[position] as SetEntry).set, items.count { it is SetEntry })
             }
         }
     }
@@ -90,11 +93,32 @@ class ExerciseSetAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(),
 //            override fun getNewListSize(): Int = items.size
 //
 //            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-//                return oldList[oldItemPosition] == items[newItemPosition]
+//                return when {
+//                    oldList[oldItemPosition] is ExerciseEntry && items[newItemPosition] is ExerciseEntry -> {
+//                        (oldList[oldItemPosition] as ExerciseEntry).exercise.name == (items[newItemPosition] as ExerciseEntry).exercise.name
+//                    }
+//
+//                    else -> {
+//                        false
+//                    }
+//                }
 //            }
 //
 //            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-//                return oldList[oldItemPosition] == items[newItemPosition]
+//                return when {
+//                    oldList[oldItemPosition] is ExerciseEntry && items[newItemPosition] is ExerciseEntry -> {
+//                        (oldList[oldItemPosition] as ExerciseEntry).exercise == (items[newItemPosition] as ExerciseEntry).exercise
+//                    }
+//
+//                    oldList[oldItemPosition] is SetEntry && items[newItemPosition] is SetEntry -> {
+//                        (oldList[oldItemPosition] as SetEntry).set == (items[newItemPosition] as SetEntry).set
+//                    }
+//
+//                    else -> {
+//                        false
+//                    }
+//                }
+//
 //            }
 //        }).dispatchUpdatesTo(this)
         notifyDataSetChanged()
@@ -106,7 +130,6 @@ class ExerciseSetAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(),
         private val addSetButton = view.findViewById<MaterialButton>(R.id.add_set)
 
         fun bind(item: ExerciseEntry) {
-            Log.d("EBIND", "e")
             title.text = item.exercise.name
 
             options.setOnClickListener {
@@ -114,7 +137,6 @@ class ExerciseSetAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(),
             }
 
             addSetButton.setOnClickListener {
-                Log.d("EBIND", "addset")
                 itemClickListener?.onAddSet(
                     item.exercise,
                     ClickableSet(Sets(type = SetType.DEFAULT.key, 0.0, 0), false)
@@ -123,7 +145,7 @@ class ExerciseSetAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(),
         }
     }
 
-    inner class SetViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class SetViewHolder(view: View) : RecyclerView.ViewHolder(view), SwipeGesture.OnSwipeListener {
         private val setIndicator = view.findViewById<MaterialTextView>(R.id.set_number)
         private val previous = view.findViewById<MaterialTextView>(R.id.previous)
         private val firstInputLayout = view.findViewById<TextInputLayout>(R.id.first_input_field)
@@ -134,9 +156,9 @@ class ExerciseSetAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(),
             view.findViewById<TextInputEditText>(R.id.second_input_field_text)
         private val check = view.findViewById<ShapeableImageView>(R.id.check)
 
-        fun bind(item: ClickableSet) {
-            setIndicator.text = "$itemCount"
-            previous.text = "" //TODO()
+        fun bind(item: ClickableSet, position: Int) {
+            setIndicator.text = "$position"
+            previous.text = "-" //TODO()
             setIndicator.setOnClickListener {
                 itemClickListener?.onSetClicked(item, itemView)
                 //TODO(Add animation where it drops down and you can change the set type)
@@ -145,6 +167,69 @@ class ExerciseSetAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(),
                 itemClickListener?.onSetCheckClicked(item, itemView)
                 //TODO(Change background and play dopamine inducing animation)
             }
+
+            val gestureDetector =
+                GestureDetector(itemView.context, object : GestureDetector.OnGestureListener {
+                    override fun onDown(e: MotionEvent): Boolean {
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onShowPress(e: MotionEvent) {
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onSingleTapUp(e: MotionEvent): Boolean {
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onScroll(
+                        e1: MotionEvent?,
+                        e2: MotionEvent,
+                        distanceX: Float,
+                        distanceY: Float
+                    ): Boolean {
+                        val diffX = e2.x - e1!!.x
+
+                        if (abs(diffX) > (itemView.width * 0.2f).toInt()) {
+                            if (diffX > 0) {
+                            } else {
+                                // LQV SWIPE
+                            }
+                            return true
+                        }
+                        return false
+                    }
+
+                    override fun onLongPress(e: MotionEvent) {
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onFling(
+                        e1: MotionEvent?,
+                        e2: MotionEvent,
+                        velocityX: Float,
+                        velocityY: Float
+                    ): Boolean {
+                        TODO("Not yet implemented")
+                    }
+
+                })
+            itemView.setOnTouchListener { _, event ->
+                gestureDetector.onTouchEvent(event)
+            }
+        }
+
+        override fun onSwipe(position: Int) {
+            if(position < 1 || position >= items.size) {
+                return
+            }
+            for (i in position downTo 0) {
+                if(items[i] is ExerciseEntry) {
+                    Log.d("ON SWIPE", "EXERCISE IS FOUND")
+                    itemClickListener?.onDeleteSet((items[i] as ExerciseEntry).exercise , (items[position] as SetEntry).set)
+                }
+            }
+
         }
     }
 
@@ -154,10 +239,6 @@ class ExerciseSetAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(),
         fun onSetCheckClicked(item: ClickableSet, clickedView: View)
         fun onAddSet(item: Exercise, set: ClickableSet)
         fun onDeleteSet(item: Exercise, set: ClickableSet)
-    }
-
-    override fun onSwipe(position: Int) {
-//        itemClickListener?.onDeleteSet(, position)
     }
 }
 
--- app/src/main/java/bg/zahov/app/util/SwipeGesture.kt
+++ app/src/main/java/bg/zahov/app/util/SwipeGesture.kt
@@ -4,7 +4,7 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 
 class SwipeGesture(private val listener: OnSwipeListener) :
-    ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+    ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
     override fun onMove(
         recyclerView: RecyclerView,
         viewHolder: RecyclerView.ViewHolder,
