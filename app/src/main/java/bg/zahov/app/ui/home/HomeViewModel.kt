10c284912b8ae8e86bd6a77cd250c646cfad5ee1
Spas Zahov <spaszahov@gmail.com>
2024-10-28 14:50:23 +0200

    renamed barData to chartData

--- app/src/main/java/bg/zahov/app/ui/home/HomeViewModel.kt
+++ app/src/main/java/bg/zahov/app/ui/home/HomeViewModel.kt
@@ -30,13 +30,13 @@ import java.time.temporal.TemporalAdjusters
  * Represents the UI state for the Home screen.
  *
  * @property numberOfWorkouts The count of workouts completed.
- * @property barData Data for the bar chart visualization.
+ * @property data Data for the bar chart visualization.
  * @property isChartLoading Indicates whether the bar chart data is still loading.
  */
 data class HomeUiState(
     val username: String = "",
     val numberOfWorkouts: String = "",
-    val barData: BarData = BarData(),
+    val data: ChartData = ChartData(),
     val isChartLoading: Boolean = true
 )
 
@@ -50,7 +50,7 @@ data class HomeUiState(
  * @property chartData The list of bar entries for the chart(BarEntry - a double (x,y) where x is where we have to place it on the x-axis and y is the value.
  * @property weekRanges The range of weeks for the X-axis labels(for example for 10/24 they would look like 7-13, 14-20 and etc.)
  */
-data class BarData(
+data class ChartData(
     var xMin: Float = 0f,
     val xMax: Float = 0f,
     val yMin: Float = 0f,
