5ac49379f069472449ab6bbae1a4937e9cc24fe6
Gotalic <vladimir.tomashevich@tumba.solutions>
2024-10-29 18:05:01 +0200

    change typesettings to use getlistofkeys

--- app/src/main/java/bg/zahov/app/data/model/Theme.kt
+++ app/src/main/java/bg/zahov/app/data/model/Theme.kt
@@ -11,7 +11,6 @@ enum class Theme(val key: String) {
 
         fun getListOfKeys(): List<String> {
             return Theme.entries.map { it.key }
-
         }
     }
 }
--- app/src/main/java/bg/zahov/app/data/model/state/TypeSettings.kt
+++ app/src/main/java/bg/zahov/app/data/model/state/TypeSettings.kt
@@ -7,13 +7,13 @@ import bg.zahov.app.data.model.Units
 
 enum class TypeSettings(val value: String, val list: List<Any>) {
     // Enums representing various settings keys.
-    LANGUAGE_SETTING("Language Settings", Language.entries.map { it.key }),
-    UNIT_SETTING("Units Settings", Units.entries.map { it.key }),
+    LANGUAGE_SETTING("Language Settings", Language.getListOfKeys()),
+    UNIT_SETTING("Units Settings", Units.getListOfKeys()),
     SOUND_EFFECTS_SETTING("soundEffectsSettings", listOf(true, false)),
-    THEME_SETTING("Theme Settings", Theme.entries.map { it.key }),
+    THEME_SETTING("Theme Settings", Theme.getListOfKeys()),
     REST_TIMER_SETTING("Reset Timer Settings", listOf(5, 15, 30)),
     VIBRATION_SETTING("vibrateSettings", listOf(true, false)),
-    SOUND_SETTING("Sound Settings", Sound.entries.map { it.key }),
+    SOUND_SETTING("Sound Settings", Sound.getListOfKeys()),
     UPDATE_TEMPLATE_SETTING("Update Template Settings", listOf(true, false)),
     WATCH_SETTINGS("Watch Settings", listOf(true, false)),
     AUTOMATIC_SYNC_SETTING("Auto Sync Settings", listOf(true, false))
