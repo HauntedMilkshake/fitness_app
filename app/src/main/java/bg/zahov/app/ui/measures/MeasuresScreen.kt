43ab3350bdc15bb4c521adb8fbcafb95ecb0dcba
Gotalic <vladimir.tomashevich@tumba.solutions>
2024-11-12 17:21:11 +0200

    added state

--- app/src/main/java/bg/zahov/app/ui/measures/MeasuresScreen.kt
+++ app/src/main/java/bg/zahov/app/ui/measures/MeasuresScreen.kt
@@ -1,42 +1,38 @@
 package bg.zahov.app.ui.measures
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import bg.zahov.app.ui.custom.CommonDivider
 
 @Composable
 fun MeasuresScreen(navigateInfo: (String) -> Unit, viewModel: MeasuresViewModel = viewModel()) {
-    MeasuresContent(
-        items = viewModel.getAllMeasures(),
-        onClick = {
-            viewModel.onMeasurementClick(it)
-            navigateInfo(it)
-        }
-    )
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    MeasuresContent(items = uiState, onClick = {
+        viewModel.onMeasurementClick(it)
+        navigateInfo(it)
+    })
 }
 
 @Composable
 fun MeasuresContent(
-    modifier: Modifier = Modifier,
-    items: List<String>,
-    onClick: (String) -> Unit
+    modifier: Modifier = Modifier, items: List<String>, onClick: (String) -> Unit
 ) {
     LazyColumn(modifier = modifier, verticalArrangement = Arrangement.spacedBy(20.dp)) {
         items(items) { item ->
--- app/src/main/java/bg/zahov/app/ui/measures/MeasuresViewModel.kt
+++ app/src/main/java/bg/zahov/app/ui/measures/MeasuresViewModel.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.viewModelScope
 import bg.zahov.app.Inject
 import bg.zahov.app.data.interfaces.MeasurementProvider
 import bg.zahov.app.data.model.MeasurementType
+import bg.zahov.app.ui.home.HomeUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 /**
@@ -18,6 +21,17 @@ import kotlinx.coroutines.launch
 class MeasuresViewModel(
     private val measurementProvider: MeasurementProvider = Inject.measurementProvider
 ) : ViewModel() {
+
+
+    //A private mutable state flow that holds a list of all measurement names as strings.
+    private val _uiState = MutableStateFlow(getAllMeasures())
+
+    /**
+     * A public state flow that exposes an immutable list of measurement names as strings to observers.
+     * Used for displaying or selecting measurement types within the UI.
+     */
+    val uiState: StateFlow<List<String>> = _uiState
+
     /**
      * Handles the click action on a measurement item.
      * Updates the UI state with the selected measurement's title and triggers
@@ -38,6 +52,6 @@ class MeasuresViewModel(
      *
      * @return A list of strings representing each measurement's title.
      */
-    fun getAllMeasures() =
+    private fun getAllMeasures() =
         enumValues<MeasurementType>().map { it.key }
 }
