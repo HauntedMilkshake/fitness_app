e5e5630457f44458d888d8802024f349b54b8050
Gotalic <vladimir.tomashevich@tumba.solutions>
2024-12-04 16:19:14 +0200

    removed imports

--- app/src/main/java/bg/zahov/app/ui/home/HomeFragment.kt
+++ app/src/main/java/bg/zahov/app/ui/home/HomeFragment.kt
@@ -2,23 +2,14 @@ package bg.zahov.app.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import bg.zahov.app.clearMenu
 import bg.zahov.app.hideTopBar
-import bg.zahov.app.setToolBarTitle
 import bg.zahov.app.showBottomNav
-import bg.zahov.app.showTopBar
 import bg.zahov.fitness.app.R
 
 class HomeFragment : Fragment() {
@@ -30,20 +21,13 @@ class HomeFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
-                HomeScreen(
-                ) { findNavController().navigate(R.id.home_to_settings) }
+                HomeScreen{ findNavController().navigate(R.id.home_to_settings) }
             }
         }
     }
 
-
     override fun onResume() {
         super.onResume()
         activity?.showBottomNav()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        activity?.clearMenu()
     }
 }
