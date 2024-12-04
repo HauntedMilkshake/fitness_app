04638604bd03b8e8d94758f0c690194425c94312
Gotalic <vladimir.tomashevich@tumba.solutions>
2024-12-04 15:57:53 +0200

    prettier ui

--- app/src/main/java/bg/zahov/app/ui/custom/CommonTopAppBar.kt
+++ app/src/main/java/bg/zahov/app/ui/custom/CommonTopAppBar.kt
@@ -1,9 +1,12 @@
 package bg.zahov.app.ui.custom
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -11,15 +14,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import bg.zahov.fitness.app.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CommonTopAppBar(
     title: Int,
+    modifier: Modifier = Modifier,
     onNavigateHome: (() -> Unit)? = null,
     dropDownMenuItems: List<(@Composable (() -> Unit) -> Unit)>? = null,
     onDropDownMenuClick: (() -> Unit)? = null
@@ -27,8 +33,14 @@ fun CommonTopAppBar(
     var showMenu by remember { mutableStateOf(false) }
 
     TopAppBar(
+        modifier = modifier,
         title = {
-            Text(text = stringResource(title), maxLines = 1, overflow = TextOverflow.Ellipsis)
+            Text(
+                text = stringResource(title),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.titleLarge
+            )
         },
         navigationIcon = {
             onNavigateHome?.let {
@@ -46,11 +58,15 @@ fun CommonTopAppBar(
             }) {
                 Icon(
                     painter = painterResource(R.drawable.ic_settings_dots),
-                    contentDescription = "Menu"
+                    contentDescription = stringResource(R.string.menu)
                 )
             }
             dropDownMenuItems?.let { items ->
                 DropdownMenu(
+                    modifier = Modifier.background(
+                        MaterialTheme.colorScheme.secondary,
+                        shape = RoundedCornerShape(8.dp)
+                    ),
                     expanded = showMenu,
                     onDismissRequest = { showMenu = false }
                 ) {
