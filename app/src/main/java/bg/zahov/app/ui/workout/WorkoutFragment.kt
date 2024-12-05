c3ebdd7e47ab36dd336cec8a0dae00cb289e8ca3
Spas Zahov <spaszahov@gmail.com>
2024-12-05 14:54:33 +0200

    gluing the vm in attempt to remove the large diff

--- app/src/main/java/bg/zahov/app/ui/workout/WorkoutFragment.kt
+++ app/src/main/java/bg/zahov/app/ui/workout/WorkoutFragment.kt
@@ -7,213 +7,148 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.core.os.bundleOf
-import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.map
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.LinearLayoutManager
-import bg.zahov.app.data.model.SetType
-import bg.zahov.app.data.model.state.OnGoingWorkoutUiMapper
+import bg.zahov.app.data.model.RestState
+import bg.zahov.app.data.model.ToastManager
 import bg.zahov.app.hideBottomNav
 import bg.zahov.app.hideTopBar
 import bg.zahov.app.showBottomNav
 import bg.zahov.app.showTopBar
+import bg.zahov.app.ui.exercise.ExercisesFragment.Companion.ADD_EXERCISE_ARG
 import bg.zahov.app.ui.exercise.ExercisesFragment.Companion.REPLACE_EXERCISE_ARG
-import bg.zahov.app.ui.exercise.ExercisesFragment.Companion.SELECT_EXERCISE_ARG
 import bg.zahov.app.ui.exercise.ExercisesFragment.Companion.STATE_ARG
-import bg.zahov.app.ui.workout.add.ExerciseSetAdapter
-import bg.zahov.app.ui.workout.add.WorkoutEntry
-import bg.zahov.app.util.SetSwipeGesture
 import bg.zahov.app.util.applyScaleAnimation
 import bg.zahov.fitness.app.R
 import bg.zahov.fitness.app.databinding.FragmentWorkoutBinding
+import kotlinx.coroutines.launch
 
 class WorkoutFragment : Fragment() {
     private var _binding: FragmentWorkoutBinding? = null
     private val binding
         get() = requireNotNull(_binding)
 
-    private val onGoingWorkoutViewModel: WorkoutViewModel by viewModels()
+    private val onGoingWorkoutViewModel: WorkoutViewModel by activityViewModels()
     private var onBackPressedCallback: OnBackPressedCallback? = null
+    private val toastManager: ToastManager = ToastManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
+        activity?.hideTopBar()
+
         _binding = FragmentWorkoutBinding.inflate(inflater, container, false)
+
+        binding.workoutComposable.setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+        binding.workoutComposable.setContent {
+            WorkoutScreen(
+                workoutViewModel = onGoingWorkoutViewModel,
+                onAddExercise = {
+                    findNavController().navigate(
+                        R.id.workout_to_add_exercise,
+                        bundleOf(STATE_ARG to ADD_EXERCISE_ARG)
+                    )
+                },
+                onReplaceExercise = {
+                    findNavController().navigate(
+                        R.id.workout_to_add_exercise,
+                        bundleOf(STATE_ARG to REPLACE_EXERCISE_ARG)
+                    )
+                },
+                onBackPressed = {
+                    if (findNavController().currentDestination?.id == R.id.workout) {
+                        onGoingWorkoutViewModel.minimize()
+                        activity?.showBottomNav()
+                    }
+                    findNavController().navigateUp()
+                },
+                onCancel = {
+                    findNavController().navigateUp()
+                }
+            )
+        }
+
         return binding.root
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val inflater = TransitionInflater.from(requireContext())
         enterTransition = inflater.inflateTransition(R.transition.slide_up)
         exitTransition = inflater.inflateTransition(R.transition.fade_out)
-        requireActivity().hideBottomNav()
+        activity?.hideBottomNav()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        requireActivity().hideTopBar()
+        activity?.hideTopBar()
         binding.apply {
-            onGoingWorkoutViewModel.restTimer.map { OnGoingWorkoutUiMapper.map(it) }
-                .observe(viewLifecycleOwner) {
-                    restTimerIndicator.visibility = it.restTimerVisibility
-                    restTimerCounter.visibility = it.restTimerVisibility
-                    restTimerCounter.text = it.rest
-                    it.message?.let { message ->
-                        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-                    }
-                }
 
-            val exerciseSetAdapter = ExerciseSetAdapter().apply {
-                itemClickListener = object : ExerciseSetAdapter.ItemClickListener<WorkoutEntry> {
-                    override fun onAddSet(itemPosition: Int) {
-                        onGoingWorkoutViewModel.addSet(itemPosition)
-                    }
-
-                    override fun onNoteToggle(itemPosition: Int) {
-                        onGoingWorkoutViewModel.toggleExerciseNoteField(itemPosition)
+            lifecycleScope.launch {
+                viewLifecycleOwner.repeatOnLifecycle(state = Lifecycle.State.STARTED) {
+                    launch {
+                        toastManager.messages.collect {
+                            it?.let { message ->
+                                Toast.makeText(
+                                    context,
+                                    context?.getString(message.messageResId),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                            onGoingWorkoutViewModel.clearToast()
+                        }
                     }
 
-                    override fun onReplaceExercise(itemPosition: Int) {
-                        onGoingWorkoutViewModel.onExerciseReplace(itemPosition)
-                        findNavController().navigate(
-                            R.id.workout_to_add_exercise,
-                            bundleOf(STATE_ARG to REPLACE_EXERCISE_ARG)
-                        )
-                    }
+                    launch {
+                        onGoingWorkoutViewModel.uiState.collect {
+                            timer.text = it.timer
+                            restTimerIndicator.visibility =
+                                if (it.restState == RestState.Active) View.VISIBLE else View.GONE
+                            restTimerCounter.visibility =
+                                if (it.restState == RestState.Active) View.VISIBLE else View.GONE
+                            restTimerCounter.text = it.restTimer
 
-                    override fun onRemoveExercise(itemPosition: Int) {
-                        onGoingWorkoutViewModel.removeExercise(itemPosition)
-                    }
-
-                    override fun onSetTypeChanged(itemPosition: Int, setType: SetType) {
-                        onGoingWorkoutViewModel.onSetTypeChanged(itemPosition, setType)
-                    }
-
-                }
-                swipeActionListener = object : ExerciseSetAdapter.SwipeActionListener {
-                    override fun onDeleteSet(itemPosition: Int) {
-                        onGoingWorkoutViewModel.removeSet(itemPosition)
-                    }
-
-                }
-                textChangeListener = object : ExerciseSetAdapter.TextActionListener {
-                    override fun onInputFieldChanged(
-                        itemPosition: Int,
-                        metric: String,
-                        id: Int,
-                    ) {
-                        onGoingWorkoutViewModel.onInputFieldChanged(itemPosition, metric, id)
-                    }
-
-                    override fun onNoteChanged(itemPosition: Int, text: String) {
-                        onGoingWorkoutViewModel.changeNote(itemPosition, text)
+                            if (it.isFinished) findNavController().navigate(R.id.workout_to_finish_workout)
+                        }
                     }
                 }
             }
-
-            exercisesRecyclerView.apply {
-                adapter = exerciseSetAdapter
-                layoutManager = LinearLayoutManager(requireContext())
-            }
-
-            ItemTouchHelper(SetSwipeGesture()).attachToRecyclerView(exercisesRecyclerView)
-
-            onGoingWorkoutViewModel.exercises.observe(viewLifecycleOwner) {
-                exerciseSetAdapter.updateItems(it)
-            }
-
-            onGoingWorkoutViewModel.name.observe(viewLifecycleOwner) {
-                workoutName.text = it
-            }
-
-            onGoingWorkoutViewModel.note.observe(viewLifecycleOwner) {
-                workoutNoteField.hint = ""
-                workoutNoteFieldText.setText(it)
-            }
-
-            workoutNoteFieldText.setOnFocusChangeListener { _, hasFocus ->
-                if (hasFocus) {
-                    workoutNoteField.hint = ""
-                    workoutNoteFieldText.hint = ""
-                }
-            }
-            workoutNoteFieldText.addTextChangedListener {
-//                onGoingWorkoutViewModel.setWorkoutNote(it.toString())
-            }
-            addExercise.setOnClickListener {
-                findNavController().navigate(
-                    R.id.workout_to_add_exercise,
-                    bundleOf(STATE_ARG to SELECT_EXERCISE_ARG)
-                )
-            }
-
-            onGoingWorkoutViewModel.timer.observe(viewLifecycleOwner) {
-                timer.text = it
-            }
-
             minimize.setOnClickListener {
                 it.applyScaleAnimation()
                 onGoingWorkoutViewModel.minimize()
-                requireActivity().showBottomNav()
+                activity?.showBottomNav()
                 findNavController().navigateUp()
             }
-
-            cancel.setOnClickListener {
-                it.applyScaleAnimation()
-                onGoingWorkoutViewModel.cancel()
-                requireActivity().showBottomNav()
-                findNavController().navigateUp()
-
-            }
-
             restTimer.setOnClickListener {
                 it.applyScaleAnimation()
                 findNavController().navigate(R.id.workout_to_rest_timer)
             }
-
             finishText.setOnClickListener {
                 it.applyScaleAnimation()
                 onGoingWorkoutViewModel.finishWorkout()
-                requireActivity().showBottomNav()
-
             }
-            onGoingWorkoutViewModel.navigate.observe(viewLifecycleOwner) {
-                it?.let { findNavController().navigate(it) }
-            }
-            onBackPressedCallback = object : OnBackPressedCallback(true) {
-                override fun handleOnBackPressed() {
-                    if (findNavController().currentDestination?.id == R.id.workout) {
-                        onGoingWorkoutViewModel.minimize()
-                        requireActivity().showBottomNav()
-                    }
-                    findNavController().navigateUp()
-                }
-            }
-
-            activity?.onBackPressedDispatcher?.addCallback(
-                viewLifecycleOwner,
-                onBackPressedCallback!!
-            )
         }
     }
 
     override fun onResume() {
         super.onResume()
-        requireActivity().hideTopBar()
-        requireActivity().hideBottomNav()
+        activity?.hideTopBar()
+        activity?.hideBottomNav()
     }
 
     override fun onStop() {
         super.onStop()
-        requireActivity().showTopBar()
-        requireActivity().showBottomNav()
+        activity?.showTopBar()
+        activity?.showBottomNav()
     }
 
     override fun onDestroy() {
