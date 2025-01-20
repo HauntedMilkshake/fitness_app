5359c10097104446ed86fac58557d1a471fd32a0
Spas Zahov <spaszahov@gmail.com>
2025-01-20 10:39:14 +0200

    kdoc

--- app/src/main/java/bg/zahov/app/ui/history/info/topbar/HistoryInfoTopBarViewModel.kt
+++ app/src/main/java/bg/zahov/app/ui/history/info/topbar/HistoryInfoTopBarViewModel.kt
@@ -1,16 +1,27 @@
 package bg.zahov.app.ui.history.info.topbar
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import bg.zahov.app.Inject
 import bg.zahov.app.data.interfaces.HistoryInfoActionHandler
 
+/**
+ * ViewModel for handling actions related to the top bar in the History Info screen.
+ *
+ * @property historyInfoTopBarHandler defines the actions to be performed when interacting with the top bar
+ */
 class HistoryInfoTopBarViewModel(private val historyInfoTopBarHandler: HistoryInfoActionHandler = Inject.historyInfoTopAppHandler) :
     ViewModel() {
+
+    /**
+     * Triggers the delete action
+     */
     fun triggerDelete() {
         historyInfoTopBarHandler.triggerDelete()
     }
 
+    /**
+     * Triggers the save action
+     */
     fun triggerSave() {
         historyInfoTopBarHandler.triggerSave()
     }
