717c2bbdedc448b8b14e2bc34aaa855b6394f9d8
Spas Zahov <spaszahov@gmail.com>
2025-01-21 11:14:34 +0200

    Revert "fixed merge conflict"
    This reverts commit 4d908092fcb97934746b8fbc2b63b364c3d960be, reversing
    changes made to 44b8601a63f35738c37d729806c60e75a7bc30a5.

--- app/src/main/java/bg/zahov/app/ui/loading/LoadingFragment.kt
+++ app/src/main/java/bg/zahov/app/ui/loading/LoadingFragment.kt
@@ -4,36 +4,81 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.ViewCompositionStrategy
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.map
 import androidx.navigation.fragment.findNavController
+import bg.zahov.app.data.model.state.LoadingUiMapper
 import bg.zahov.app.hideBottomNav
-import bg.zahov.fitness.app.R
+import bg.zahov.fitness.app.databinding.FragmentLoadingBinding
+import com.google.android.material.imageview.ShapeableImageView
 
 class LoadingFragment : Fragment() {
+    private var _binding: FragmentLoadingBinding? = null
+    private val binding
+        get() = requireNotNull(_binding)
+    private val loadingViewModel: LoadingViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        return ComposeView(requireContext()).apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-            setContent {
-                LoadingScreen(
-                    navigateWelcome = {
-                        findNavController().navigate(R.id.loading_to_welcome)
-                    },
-                    navigateHome = {
-                        findNavController().navigate(R.id.loading_to_home)
-                    }
-                )
-            }
-        }
+        _binding = FragmentLoadingBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onStart() {
         super.onStart()
-        activity?.hideBottomNav()
+        requireActivity().hideBottomNav()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            animateView(bottomLeft)
+            animateView(bottomRight)
+            animateView(topLeft)
+            animateView(topRight)
+            loadingViewModel.onAppStart()
+
+            loadingViewModel.state.map { LoadingUiMapper.map(it) }.observe(viewLifecycleOwner) {
+                it.message?.let { message ->
+                    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+                }
+                it.destination?.let { des ->
+                    findNavController().navigate(des)
+                }
+            }
+        }
+    }
+
+    private fun animateView(view: ShapeableImageView) {
+        val scaleAnimation = ScaleAnimation(
+            0f, 10f,
+            0f, 10f,
+            Animation.RELATIVE_TO_SELF, 0.5f,
+            Animation.RELATIVE_TO_SELF, 0.5f
+        )
+
+        scaleAnimation.duration = 2000L
+        scaleAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+        })
+        view.startAnimation(scaleAnimation)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/bg/zahov/app/ui/loading/LoadingViewModel.kt
+++ app/src/main/java/bg/zahov/app/ui/loading/LoadingViewModel.kt
@@ -1,61 +1,49 @@
 package bg.zahov.app.ui.loading
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import bg.zahov.app.Inject
-import bg.zahov.app.data.interfaces.ServiceErrorHandler
-import bg.zahov.app.data.interfaces.UserProvider
+import bg.zahov.app.getServiceErrorProvider
+import bg.zahov.app.getUserProvider
+import bg.zahov.fitness.app.R
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
-/**
- * ViewModel responsible for managing the app's loading process at startup.
- *
- * This ViewModel is designed to handle the logic required to determine the next step of the app:
- * whether to proceed to the home screen or display the welcome screen.
- * It also ensures robust error handling during the loading process.
- *
- * @property userProvider A service that provides user-related functionality, such as authentication
- *                        checks and user data initialization. Injected by default via `Inject.userProvider`.
- * @property serviceError A handler for managing errors and service-related issues during the startup
- *                        process. Injected by default via `Inject.serviceErrorHandler`.
- */
-class LoadingViewModel(
-    private val userProvider: UserProvider = Inject.userProvider,
-    private val serviceError: ServiceErrorHandler = Inject.serviceErrorHandler
-) : ViewModel() {
+class LoadingViewModel(application: Application) : AndroidViewModel(application) {
+    private val userProvider by lazy {
+        application.getUserProvider()
+    }
+    private val serviceError by lazy {
+        application.getServiceErrorProvider()
+    }
+    private val _state = MutableLiveData<State>()
+    val state: LiveData<State>
+        get() = _state
 
-    /**
-     * Executes the startup logic.
-     *
-     * This function is responsible for:
-     * - Checking the user's authentication status.
-     * - Initializing data sources and fetching user data if the user is authenticated.
-     * - Navigating to the home screen if authentication is successful.
-     * - Navigating to the welcome screen if the user is not authenticated or a `NoSuchElementException` occurs.
-     * - Handling unexpected errors by initiating the service error countdown.
-     *
-     * @param pass A lambda function to be executed when loading is successful and the app should
-     *             navigate to the home screen.
-     * @param failed A lambda function to be executed when loading fails or when the app should
-     *               navigate to the welcome screen.
-     */
-    fun loading(pass: () -> Unit, failed: () -> Unit) {
+    fun onAppStart() {
         viewModelScope.launch {
             try {
                 if (userProvider.isAuthenticated()) {
                     userProvider.initDataSources()
                     userProvider.getUser().first()
-                    pass()
+                    _state.postValue(State.Navigate(R.id.loading_to_home))
                 } else {
-                    failed()
+                    _state.postValue(State.Navigate(R.id.loading_to_welcome))
                 }
             } catch (e: Exception) {
                 when (e) {
-                    is NoSuchElementException -> failed()
+                    is NoSuchElementException -> _state.postValue(State.Navigate(R.id.loading_to_welcome))
                     else -> serviceError.initiateCountdown()
                 }
             }
         }
+    }
+
+    sealed interface State {
+        data class Loading(val isDataLoading: Boolean) : State
+        data class Navigate(val destination: Int?) : State
     }
 }
