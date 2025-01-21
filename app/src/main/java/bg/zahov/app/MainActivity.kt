692f57b373fdac5309da902576613164099464a6
Gotalic <vladimir.tomashevich@tumba.solutions>
2025-01-21 17:06:05 +0200

    added edge to edge

--- app/src/main/java/bg/zahov/app/MainActivity.kt
+++ app/src/main/java/bg/zahov/app/MainActivity.kt
@@ -2,6 +2,7 @@ package bg.zahov.app
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private val loadingViewModel: LoadingViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         splashScreen.setKeepOnScreenCondition {
