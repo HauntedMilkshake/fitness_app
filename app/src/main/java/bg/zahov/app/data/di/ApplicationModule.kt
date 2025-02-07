56d0da017d073dae5d765dc032a13984596552fd
Spas Zahov <spaszahov@gmail.com>
2025-02-07 14:17:43 +0200

    replaced ActivityModule Install in from ActivityComponent to RetainedComponent

--- app/src/main/java/bg/zahov/app/data/di/ApplicationModule.kt
+++ app/src/main/java/bg/zahov/app/data/di/ApplicationModule.kt
@@ -24,12 +24,12 @@ import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ActivityRetainedComponent
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(ActivityRetainedComponent::class)
 abstract class ActivityModule {
 
     @Binds
