fa46c178a63b37ac22a04cedb335eeff9637b716
Vladimir Tomashevich <74720763+Gotalicp@users.noreply.github.com>
2025-03-04 18:41:16 +0200

    Added base for the baseline profiles (#127)
    * added base
    
    * removed unessery testtag
    
    * added git ignore
    
    * Delete app/src/release/generated/baselineProfiles/startup-prof.txt
    
    * Delete app/src/release/generated/baselineProfiles/baseline-prof.txt
    
    ---------
    
    Co-authored-by: Gotalic <vladimir.tomashevich@tumba.solutions>

---
+++ app/baselineprofile/build.gradle.kts
@@ -0,0 +1,66 @@
+import com.android.build.api.dsl.ManagedVirtualDevice
+
+plugins {
+    alias(libs.plugins.android.test)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.baselineprofile)
+}
+
+android {
+    namespace = "com.example.baselineprofile"
+    compileSdk = 35
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    defaultConfig {
+        minSdk = 28
+        targetSdk = 35
+        testInstrumentationRunnerArguments["androidx.benchmark.dryRunMode.enable"] = "true"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    targetProjectPath = ":app"
+
+    // This code creates the gradle managed device used to generate baseline profiles.
+    // To use GMD please invoke generation through the command line:
+    // ./gradlew :app:generateBaselineProfile
+    testOptions.managedDevices.devices {
+        create<ManagedVirtualDevice>("mediumphoneApi35") {
+            device = "Medium Phone"
+            apiLevel = 35
+            systemImageSource = "google"
+        }
+    }
+}
+
+// This is the configuration block for the Baseline Profile plugin.
+// You can specify to run the generators on a managed devices or connected devices.
+baselineProfile {
+    managedDevices += "mediumphoneApi35"
+    useConnectedDevices = false
+}
+
+dependencies {
+    implementation(libs.androidxTestExtJunit)
+    implementation(libs.espressoCore)
+    implementation(libs.androidx.uiautomator)
+    implementation(libs.androidx.benchmark.macro.junit4)
+}
+
+androidComponents {
+    onVariants { v ->
+        val artifactsLoader = v.artifacts.getBuiltArtifactsLoader()
+        v.instrumentationRunnerArguments.put(
+            "targetAppId",
+            v.testedApks.map { artifactsLoader.load(it)?.applicationId }
+        )
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -12,4 +12,6 @@ plugins {
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.android.test) apply false
+    alias(libs.plugins.baselineprofile) apply false
 }
