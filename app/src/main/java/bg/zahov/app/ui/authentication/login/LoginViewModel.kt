f85d9e50670d54c35b25093d023d6a0a734d2463
Spas Zahov <spaszahov@gmail.com>
2025-03-20 13:03:51 +0200

    removed capturing auth in variable

--- app/src/main/java/bg/zahov/app/ui/authentication/login/LoginViewModel.kt
+++ app/src/main/java/bg/zahov/app/ui/authentication/login/LoginViewModel.kt
@@ -83,7 +83,7 @@ class LoginViewModel @Inject constructor(
     fun login() {
         viewModelScope.launch {
             try {
-                val res = auth.login(_uiState.value.email, _uiState.value.password)
+                auth.login(_uiState.value.email, _uiState.value.password)
             } catch (e: Exception) {
                 when (e) {
                     is FirebaseAuthInvalidUserException -> {
